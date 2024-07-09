412d8506184184fad094b1645a6bd6cd3222a4a6
MIchiel Koning <michiel.koning@valtech.com>
2024-07-09 11:00:58 +0200

    linter

--- src/Views/Recipes/RecipesEdit.tsx
+++ src/Views/Recipes/RecipesEdit.tsx
@@ -1,8 +1,7 @@
 import { FunctionComponent } from "react";
 import { Edit, EditProps, SimpleForm, TextInput, required } from "react-admin";
 
-
-    export const RecipesEdit: FunctionComponent<EditProps> = () => (
+export const RecipesEdit: FunctionComponent<EditProps> = () => (
   <Edit>
     <SimpleForm>
       <TextInput source="title" validate={required()} />
--- src/Views/Recipes/index.tsx
+++ src/Views/Recipes/index.tsx
@@ -1,15 +1,15 @@
-import { ResourceProps } from 'react-admin';
-import DescriptionIcon from '@mui/icons-material/Description';
-import { RecipesEdit } from './RecipesEdit';
-import { RecipesList } from './RecipesList';
-import { RecipesShow } from './RecipesShow';
+import { ResourceProps } from "react-admin";
+import DescriptionIcon from "@mui/icons-material/Description";
+import { RecipesEdit } from "./RecipesEdit";
+import { RecipesList } from "./RecipesList";
+import { RecipesShow } from "./RecipesShow";
 
 const RecipeRecourse: ResourceProps = {
   name: "recipes",
   list: RecipesList,
   show: RecipesShow,
   edit: RecipesEdit,
-  icon: DescriptionIcon
+  icon: DescriptionIcon,
 };
 
-export default RecipeRecourse
+export default RecipeRecourse;
