1aab47d26239fcbc40156ed2a444371e1bee7c73
Michiel Koning <mail@michielkoning.nl>
2024-07-09 11:48:03 +0200

    pwa

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
+console.log(import.meta.env.VITE_SUPABASE_URL);
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
