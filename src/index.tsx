0071b850822c9b209fc4584b0dc06db7e0d9677a
Michiel Koning <mail@michielkoning.nl>
2024-07-09 12:16:48 +0200

    restore pwa

--- src/index.tsx
+++ src/index.tsx
@@ -10,8 +10,3 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     <App />
   </React.StrictMode>,
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
