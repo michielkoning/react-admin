fd889ce74ad83a6c81420ff7947ba736637ccba8
Michiel Koning <mail@michielkoning.nl>
2024-07-09 17:55:22 +0200

    base

--- src/App.tsx
+++ src/App.tsx
@@ -3,31 +3,27 @@ import { Admin, CustomRoutes, Resource } from "react-admin";
 import { LoginPage, SetPasswordPage, ForgotPasswordPage } from "ra-supabase";
 import { Route, BrowserRouter } from "react-router-dom";
 import { authProvider } from "./AuthProvider/SupabaseAuthProvider";
-import OfflineNotification from "./Components/OfflineNotification";
 import { dataProvider } from "./DataProviders/SupabaseDataProvider";
 import { i18nProvider } from "./I18n/I18nProvider";
 import Recipes from "./Views/Recipes";
 
 export const App: FunctionComponent = () => (
-  <>
-    <BrowserRouter>
-      <Admin
-        dataProvider={dataProvider}
-        authProvider={authProvider}
-        loginPage={LoginPage}
-        i18nProvider={i18nProvider}
-        disableTelemetry
-      >
-        <CustomRoutes noLayout>
-          <Route path={SetPasswordPage.path} element={<SetPasswordPage />} />
-          <Route
-            path={ForgotPasswordPage.path}
-            element={<ForgotPasswordPage />}
-          />
-        </CustomRoutes>
-        <Resource {...Recipes} />
-      </Admin>
-    </BrowserRouter>
-    <OfflineNotification />
-  </>
+  <BrowserRouter>
+    <Admin
+      dataProvider={dataProvider}
+      authProvider={authProvider}
+      loginPage={LoginPage}
+      i18nProvider={i18nProvider}
+      disableTelemetry
+    >
+      <CustomRoutes noLayout>
+        <Route path={SetPasswordPage.path} element={<SetPasswordPage />} />
+        <Route
+          path={ForgotPasswordPage.path}
+          element={<ForgotPasswordPage />}
+        />
+      </CustomRoutes>
+      <Resource {...Recipes} />
+    </Admin>
+  </BrowserRouter>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -41,5 +41,4 @@ export default defineConfig({
   server: {
     host: true,
   },
-  base: "./",
 });
