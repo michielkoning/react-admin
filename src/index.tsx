120ff5cccda13199abfe1db5ff39e7ba50dbe878
Michiel Koning <mail@michielkoning.nl>
2024-07-09 12:34:41 +0200

    vite pwa

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
-import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
-
-console.log(import.meta.env.VITE_BASE_URL);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,46 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    define: {
-        'process.env': process.env,
-    },
-    server: {
-        host: true,
-    },
-    base: './',
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: false,
+
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
+
+      manifest: {
+        name: "my-vue-app",
+        short_name: "my-vue-app",
+        description: "my-vue-app",
+        theme_color: "#ffffff",
+      },
+
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
+
+      devOptions: {
+        enabled: false,
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+        type: "module",
+      },
+    }),
+  ],
+  define: {
+    "process.env": process.env,
+  },
+  server: {
+    host: true,
+  },
+  base: "./",
 });
