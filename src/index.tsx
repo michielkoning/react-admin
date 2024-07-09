32c70e61b5991ec58fc4edbec748359ffc028115
Michiel Koning <mail@michielkoning.nl>
2024-07-09 11:54:01 +0200

    base

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
-console.log(import.meta.env.VITE_SUPABASE_URL);
+console.log(import.meta.env.VITE_BASE_URL);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
